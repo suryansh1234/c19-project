64eb72200d1934271a16ba48654895502b14378e
suryansh1234 <69800733+suryansh1234@users.noreply.github.com>
2020-12-24 10:13:44 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,74 +1,57 @@
+
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+const Body = Matter.Body;
 
-var engine, world;
-var box1, pig1;
-var backgroundImg,platform;
-var bird, slingShot;
-
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+var paper;
+var ground;
+var log;
+function preload()
+{
+	logImage=loadImage("sprites/dustbingreen.png");
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+function setup() {
+	createCanvas(800, 700);
 
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
+	engine = Engine.create();
+	world = engine.world;
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
+	//Create the Bodies Here.
+	ground= new Ground(400,670,800,20);
+	log=new Log(650,630,15,70);
+	log2=new Log(750,630,15,70);
+	log3=new Log(700,657,100,15);
+	paper=new Paper(100,500,18);
 
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(100,100);
-
-   // log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,{x:200,y:100});
+	Engine.run(engine);
+  
 }
 
-function draw(){
-    background(backgroundImg);
-    Engine.update(engine);
-    strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    log1.display();
 
-    box3.display();
-    box4.display();
-    pig3.display();
-    log3.display();
+function draw() {
+  rectMode(CENTER);
+  background(255,255,255);
 
-    box5.display();
-    log4.display();
-    log5.display();
 
-    bird.display();
-    platform.display();
-    //log6.display();
-    chain.display();    
+  ground.display();
+  log.display();
+  log2.display();
+  log3.display();
+  image(logImage,625,575,150,100);
+  imageMode(CENTER)
+  paper.display();
+  
+  drawSprites();
+  
+ 
 }
-function mouseDragged(){
-    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY})
+
+function keyPressed(){
+if (keyCode===UP_ARROW){
+	Matter.Body.applyForce(paper.body,paper.body.position,{x:20,y:-50})
 }
-function mouseReleased() {
-   chain.fly(); 
 }
+
