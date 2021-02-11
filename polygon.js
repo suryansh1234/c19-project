1adf5ffbe043120576a3ab76e3a1469da3ecffa0
suryansh1234 <69800733+suryansh1234@users.noreply.github.com>
2021-02-11 18:50:37 +0530

    Add files via upload

--- polygon.js
+++ polygon.js
@@ -1,22 +1,17 @@
-class Log {
+class Polygon {
     constructor(x,y,width,height) {
       var options = {
-          isStatic: false
+          isStatic: true
       }
       this.body = Bodies.rectangle(x,y,width,height,options);
       this.width = width;
       this.height = height;
-     this.image = loadImage("th.jpg");
-      
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill("red");
-      
+      fill("yellow");
       rect(pos.x, pos.y, this.width, this.height);
-      image(this.image,this.body.position.x,this.body.position.y,50,50)
-      imageMode(CENTER)
     }
-  };
+  }
--- sketch.js
+++ sketch.js
@@ -1,57 +1,55 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-
-var paper;
-var ground;
-var log;
+var ground1
 function preload()
 {
-	logImage=loadImage("sprites/dustbingreen.png");
+	
 }
 
-function setup() {
-	createCanvas(800, 700);
-
+function setup(){
+    createCanvas(800, 700);
 
 	engine = Engine.create();
-	world = engine.world;
+    world = engine.world;
+    ground1=new Ground(400,400,500,10)
+    log1=new Log(400,100,50,50)
+    log2=new Log(400,310,50,50)
+    log3=new Log(400,290,50,50)
+    log4=new Log(400,260,50,50)
+    log5=new Log(200,260,50,50)
+    log6=new Log(200,100,50,50);
 
-	//Create the Bodies Here.
-	ground= new Ground(400,670,800,20);
-	log=new Log(650,630,15,70);
-	log2=new Log(750,630,15,70);
-	log3=new Log(700,657,100,15);
-	paper=new Paper(100,500,18);
-
-	Engine.run(engine);
-  
+    polygon1=new Polygon(200,200,20,20)
+   rope1=new Chain(polygon1.body,{x:50,y:200})
+    
+    console.log(ground1.body)
+Engine.run(engine);
 }
 
-
-function draw() {
+function draw(){
   rectMode(CENTER);
   background(255,255,255);
+ground1.display();
+log1.display();
+log2.display();
+log3.display();
+log4.display();
+log5.display()
+log6.display();
+rope1.display();
+polygon1.display();
 
 
-  ground.display();
-  log.display();
-  log2.display();
-  log3.display();
-  image(logImage,625,575,150,100);
-  imageMode(CENTER)
-  paper.display();
-  
-  drawSprites();
-  
- 
+
+
+  drawSprites();  
 }
 
-function keyPressed(){
-if (keyCode===UP_ARROW){
-	Matter.Body.applyForce(paper.body,paper.body.position,{x:20,y:-50})
+function mouseReleased() {
+  rope1.fly(); 
 }
+function mouseDragged(){
+  Matter.Body.setPosition(log7.body,{x:mouseX,y:mouseY})
 }
-
